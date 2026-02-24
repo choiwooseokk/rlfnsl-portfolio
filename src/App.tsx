bc8f0c81bd6663879c66610feadccbbcb13189e2
choiwooseok <48667293+choiwooseokk@users.noreply.github.com>
2026-02-25 02:50:22 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <header style={{ padding: 16, fontSize: 20, fontWeight: 700 }}>
-        최우석 포트폴리오r
+        최우석 포트폴리오
       </header>
 
       <div className="App">
